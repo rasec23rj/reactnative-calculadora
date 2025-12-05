ca89ccf8b73323b0bd829daa357868a6d905cb30
rasec23rj <rasec23rj@gmail.com>
2025-12-05 09:52:40 -0300

    feat: methods the operation

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const initialState = {
   displayValue: '0',
   clearDisplay: false,
   operation: null,
-  value: [0,0],
+  values: [0,0],
   current: 0,
 }
 
@@ -18,16 +18,16 @@ export default class App extends Component {
   state = {...initialState  }
 
   addDigit = n => {
-    if (n == '.' && this.state.displayValue.includes('.')) {
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    if (n == '.' && !clearDisplay && this.state.displayValue.includes('.')) {
       return
     }
-    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({displayValue, clearDisplay: false})
     if (n !== '.') {
       const newValue = parseFloat(displayValue)
-      const values = [...this.state.value]
+      const values = [...this.state.values]
       values[this.state.current] = newValue
       this.setState({values})
     }
@@ -39,7 +39,26 @@ export default class App extends Component {
 
 
   setOperation = operation => {
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    }else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (error) {
+        values[0] = this.state.value[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0: 1,
+        clearDisplay: true,
+        values,
+      })
+    }
   }
   render() {
     return (
